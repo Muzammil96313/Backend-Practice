cdad0ee5175213212ab3ad78cb7ae8cbedbb070d
Muzammil96313 <muzammil.hassan.dev7@gmail.com>
2024-12-17 20:24:11 +0500

    n

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.post("/", async (req, res) => {
 //   }
 // });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+// app.listen(3000, () => {
+//   console.log("Server is running on port 3000");
+// });
 module.exports = app;
